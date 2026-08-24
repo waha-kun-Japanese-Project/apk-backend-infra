8a70fec4396f4aaf73f08960fc84f46a7f1eaba8
Mohamed <mobyb0045@gmail.com>
2026-08-24 22:19:35 +0300

    update the health core for the pods running

--- AuthService/AuthService/Program.cs
+++ AuthService/AuthService/Program.cs
@@ -6,23 +6,19 @@ using CommanLib.DependencyInjection;
 using FirebaseAdmin;
 using Google.Apis.Auth.OAuth2;
 
-
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 namespace Auth_Services
 {
     public class Program
     {
         public static async Task Main(string[] args)
         {
-
             var builder = WebApplication.CreateBuilder(args);
+
             var firebasePath = Path.Combine(
                 builder.Environment.ContentRootPath,
                 "FireBase",
-                "graduation-project-3c67f-firebase-adminsdk-fbsvc-b88880ea28.json");
+                "graduation-project-3c67f-firebase-adminsdk-fbsvc-b88880ea28.json"
+            );
 
             FirebaseApp.Create(new AppOptions
             {
@@ -30,56 +26,22 @@ namespace Auth_Services
             });
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddHealthChecks();
+
             builder.Services.AddPersistenceServices(builder.Configuration);
-
             builder.Services.AddTokenService(builder.Configuration);
-            //FirebaseApp.Create(new AppOptions
-            //{
-            //    Credential = GoogleCredential.FromFile("F:\\Graduation_Project\\AuthService\\AuthService\\Firebase\\firebase-adminsdk.json")
-            //});
-            // CHANGED: was AddServices() with no args - ServiceExtensions.AddServices
-            // now needs IConfiguration to read RabbitMq:Host/Port/Username/Password
-            // instead of hardcoding "localhost"/"guest"/"guest".
             builder.Services.AddServices(builder.Configuration);
-            //builder.Services.Configure<MailSettings>(builder.Configuration.GetSection("MailSetting"));
-
-            //builder.Services.AddTokenService(builder.Configuration);
-
-            //builder.Services.Configure<JwtOption>(builder.Configuration.GetSection(JwtOption.SectionName));
-
-            //builder.Services.AddAuthentication(options =>
-            //{
-            //    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            //    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            //}).AddJwtBearer(options =>
-            //{
-            //    var jwt = builder.Configuration.GetSection(JwtOption.SectionName).Get<JwtOption>();
-            //    options.TokenValidationParameters = new TokenValidationParameters
-            //    {
-            //        ValidateIssuer = true,
-            //        ValidateAudience = true,
-            //        ValidateIssuerSigningKey = true,
-            //        ValidateLifetime = true,
-            //        ValidIssuer = jwt.Issuer,
-            //        ValidAudience = jwt.Audience,
-            //        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt.SecretKey))
-            //    };
-            //});
-          
 
             var app = builder.Build();
 
+            using (var scope = app.Services.CreateScope())
+            {
+                var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+                await dbInitializer.InitializeAsync();
+            }
 
-            //dataseed
-
-            var scape = app.Services.CreateScope();
-            var dbInitializer = scape.ServiceProvider.GetRequiredService<IDbInitializer>();
-            await dbInitializer.InitializeAsync();
-
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
@@ -87,10 +49,11 @@ namespace Auth_Services
             }
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles(); // serves wwwroot/uploads/* (profile pictures) as static content
+            app.UseStaticFiles();
             app.UseAuthentication();
             app.UseAuthorization();
 
+            app.MapHealthChecks("/health");
             app.MapControllers();
 
             app.Run();
